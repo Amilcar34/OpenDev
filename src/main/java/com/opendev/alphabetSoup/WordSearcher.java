829f3d509b4d5a37b772106b95d93925aa62cd37
alberino-l <amilcaralberino@medife.com.ar>
2023-03-08 20:20:33 -0300

    init commit

---
+++ src/main/java/com/opendev/alphabetSoup/WordSearcher.java
@@ -0,0 +1,14 @@
+package com.opendev.alphabetSoup;
+
+public class WordSearcher {
+
+    private char soup[][];
+
+    public WordSearcher(char soup[][]) {
+        this.soup = soup;
+    }
+
+    public boolean isPresent(String word) {
+        return false;
+    }
+}
