718a4706515ef0aa323120ab8f68a071cb1cfe88
avalos.m <mora_m_avalos@hotmail.com>
2023-03-13 11:19:29 -0300

    first

--- src/main/java/com/opendev/sorting/Jugador.java
+++ src/main/java/com/opendev/sorting/Jugador.java
@@ -1,44 +1,53 @@
 package com.opendev.sorting;
 
-public class Jugador {
+public class Jugador implements Comparable<Jugador> {
 
-    private String nombre;
-    private int puntuacion;
-    private int perdidas;
+	private String nombre;
+	private int puntuacion;
+	private int perdidas;
 
-    public Jugador(String nombre, int puntuacion) {
-        this.nombre = nombre;
-        this.puntuacion = puntuacion;
-        this.perdidas = 0;
-    }
+	public Jugador(String nombre, int puntuacion) {
+		this.nombre = nombre;
+		this.puntuacion = puntuacion;
+		this.perdidas = 0;
+	}
 
-    public Jugador(String nombre, int puntuacion, int perdidas) {
-        this.nombre = nombre;
-        this.puntuacion = puntuacion;
-        this.perdidas = perdidas;
-    }
+	public Jugador(String nombre, int puntuacion, int perdidas) {
+		this.nombre = nombre;
+		this.puntuacion = puntuacion;
+		this.perdidas = perdidas;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public int getPuntuacion() {
-        return puntuacion;
-    }
+	public int getPuntuacion() {
+		return puntuacion;
+	}
 
-    public void setPuntuacion(int puntuacion) {
-        this.puntuacion = puntuacion;
-    }
+	public void setPuntuacion(int puntuacion) {
+		this.puntuacion = puntuacion;
+	}
 
-    public int getPerdidas() {
-        return perdidas;
-    }
+	public int getPerdidas() {
+		return perdidas;
+	}
 
-    public void setPerdidas(int perdidas) {
-        this.perdidas = perdidas;
-    }
+	public void setPerdidas(int perdidas) {
+		this.perdidas = perdidas;
+	}
+
+	@Override
+	public int compareTo(Jugador o) {
+		int a = this.getNombre().compareTo(o.getNombre());
+		if (a != 0) {
+			return a;
+		}
+		return Integer.valueOf(o.getPuntuacion()).compareTo(this.getPuntuacion());
+	}
 }
--- src/main/java/com/opendev/sorting/Sorting.java
+++ src/main/java/com/opendev/sorting/Sorting.java
@@ -1,28 +1,52 @@
 package com.opendev.sorting;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Sorting {
 
-    /**
-     * Se debe ordenar primero por puntuación de manera descendente, luego por nombre de manera ascendente.
-     *
-     * @param jugadores la lista de jugadores a ordenar
-     * @return la lista ordenada de jugadores
-     */
-    public static List<Jugador> ordenarPorPuntuacionYNombre(List<Jugador> jugadores) {
-        return null;
-    }
+	/**
+	 * Se debe ordenar primero por puntuación de manera descendente, luego por
+	 * nombre de manera ascendente.
+	 *
+	 * @param jugadores la lista de jugadores a ordenar
+	 * @return la lista ordenada de jugadores
+	 */
+	public static List<Jugador> ordenarPorPuntuacionYNombre(List<Jugador> jugadores) {
+		List<Jugador> jugadoresOrdenados = new ArrayList<>();
 
-    /**
-     * Se debe ordenar primero por puntuación de manera descendente. Cuando 2 jugadores tienen igual cantidad de puntos,
-     * el que tiene menos perdidas se lo considerara el mayor. Luego a igual puntos y perdidas se segxxuirá usando el
-     * nombre de manera ascendente.
-     *
-     * @param jugadores la lista de jugadores a ordenar
-     * @return la lista ordenada de jugadores
-     */
-    public static List<Jugador> ordenarPorPuntuacionPerdidasYNombre(List<Jugador> jugadores) {
-        return null;
-    }
+		// Comparator<Jugador> comparadorDescendente = Collections.reverseOrder();
+		// Collections.sort(jugadoresOrdenados, comparadorDescendente);
+
+		/*
+		 * Collections.sort(jugadoresOrdenados, new Comparator<Jugador>() {
+		 * 
+		 * @Override public int compare(Jugador o1, Jugador o2) { //return new
+		 * Jugador(o2.getPuntuacion().compareTo(new Jugador(o1.getPuntuacion()))); } });
+		 */
+
+		jugadoresOrdenados.sort((o1, o2) -> {
+			int a = o1.getNombre().compareTo(o2.getNombre());
+			if (a != 0) {
+				return a;
+			}
+			//return Integer.valueOf(o2.getPuntuacion().compareTo(o1.getPuntuacion()));
+			return Integer.valueOf(o2.getPuntuacion()).compareTo(o1.getPuntuacion());
+		});
+		// jugadoresOrdenados.sort(Comparator.comparing(Jugador::getPuntuacion).reversed());
+		return jugadoresOrdenados;
+	}
+
+	/**
+	 * Se debe ordenar primero por puntuación de manera descendente. Cuando 2
+	 * jugadores tienen igual cantidad de puntos, el que tiene menos perdidas se lo
+	 * considerara el mayor. Luego a igual puntos y perdidas se segxxuirá usando el
+	 * nombre de manera ascendente.
+	 *
+	 * @param jugadores la lista de jugadores a ordenar
+	 * @return la lista ordenada de jugadores
+	 */
+	public static List<Jugador> ordenarPorPuntuacionPerdidasYNombre(List<Jugador> jugadores) {
+		return null;
+	}
 }
